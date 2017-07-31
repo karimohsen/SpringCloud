fd13e8e8f007ee7daeec8398a24700496c4bb16c
karim_i35_ewd15 <karim.abdelmohsen.1992@gmail.com>
2017-07-31 10:18:28 +0200

    adding spring cloud server & client

---
+++ README.md
@@ -0,0 +1,6 @@
+# Server config
+_______________
+
+This repository contains a demo for spring cloud server and spring cloud client that uses specific config files in server
+
+Contact me : karim.abdelmohsen.1992@gmail.com
---
+++ spring-cloud-client/src/main/java/com/orange/springcloudclient/controller/LuckyWordController.java
@@ -0,0 +1,18 @@
+package com.orange.springcloudclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Karim on 7/31/2017.
+ */
+@RestController
+public class LuckyWordController {
+    @Value("${lucky-word}") String luckyWord;
+
+    @RequestMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
